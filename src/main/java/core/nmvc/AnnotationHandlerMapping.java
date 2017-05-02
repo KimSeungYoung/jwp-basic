7edecbd3402086c0096a25e1dc7b59fb5b2f72a3
philo-grammer <great1106@gmail.com>
2017-05-02 20:15:50 +0900

    AnnotationHandlerMappingTest 클래스의 3개 테스트 성공

--- src/main/java/core/nmvc/AnnotationHandlerMapping.java
+++ src/main/java/core/nmvc/AnnotationHandlerMapping.java
@@ -1,14 +1,25 @@
 package core.nmvc;
 
-import java.util.Map;
+import com.google.common.collect.Maps;
+import core.annotation.Controller;
+import core.annotation.RequestMapping;
+import core.annotation.RequestMethod;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
-
-import com.google.common.collect.Maps;
-
-import core.annotation.RequestMethod;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
+
+    private static final Logger logger = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
+
     private Object[] basePackage;
 
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
@@ -17,8 +28,20 @@ public class AnnotationHandlerMapping {
         this.basePackage = basePackage;
     }
 
-    public void initialize() {
+    public void initialize() throws Exception {
+        Reflections reflections = new Reflections(basePackage, new MethodAnnotationsScanner(), new TypeAnnotationsScanner(), new SubTypesScanner());
+        Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(Controller.class);
+        for (Class<?> annotatedClass : annotated) {
+            logger.debug("annotated class : {}", annotatedClass.getName());
+            Set<Method> methods = reflections.getMethodsAnnotatedWith(RequestMapping.class);
 
+            for (Method method : methods) {
+                RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                String value = requestMapping.value();
+                RequestMethod requestMethod = requestMapping.method();
+                handlerExecutions.put(new HandlerKey(value, requestMethod), new HandlerExecution(method, annotatedClass.newInstance()));
+            }
+        }
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
--- src/main/java/core/nmvc/HandlerExecution.java
+++ src/main/java/core/nmvc/HandlerExecution.java
@@ -1,12 +1,22 @@
 package core.nmvc;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import core.mvc.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+
 public class HandlerExecution {
+
+    public Method method;
+    public Object instance;
+
+    public HandlerExecution(Method method, Object instance) {
+        this.method = method;
+        this.instance = instance;
+    }
+
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+        return (ModelAndView) method.invoke(instance, request, response);
     }
 }
