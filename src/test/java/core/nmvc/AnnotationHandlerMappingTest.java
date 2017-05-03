10d476b2943b8ed10b4a264a70416dd39c1ddb4c
philo-grammer <great1106@gmail.com>
2017-05-03 17:05:24 +0900

    Refactoring test code (AnnotationHandlerMappingTest)

--- src/test/java/core/nmvc/AnnotationHandlerMappingTest.java
+++ src/test/java/core/nmvc/AnnotationHandlerMappingTest.java
@@ -22,55 +22,44 @@ public class AnnotationHandlerMappingTest {
 
     @Test
     public void list() throws Exception {
-        MockHttpServletRequest request = new MockHttpServletRequest("GET", "/users");
-        HandlerExecution execution = handlerMapping.getHandler(request);
-        ModelAndView mav = execution.handle(request, response);
-        mav.getView().render(mav.getModel(), request, response);
+        executeTestWith("GET", "/users");
         assertEquals("/users/list.jsp", response.getForwardedUrl());
     }
 
     @Test
     public void show() throws Exception {
-        MockHttpServletRequest request = new MockHttpServletRequest("GET", "/users/show");
-        HandlerExecution execution = handlerMapping.getHandler(request);
-        ModelAndView mav = execution.handle(request, response);
-        mav.getView().render(mav.getModel(), request, response);
+        executeTestWith("GET", "/users/show");
         assertEquals("/users/show.jsp", response.getForwardedUrl());
     }
 
     @Test
     public void create() throws Exception {
-        MockHttpServletRequest request = new MockHttpServletRequest("POST", "/users");
-        HandlerExecution execution = handlerMapping.getHandler(request);
-        ModelAndView mav = execution.handle(request, response);
-        mav.getView().render(mav.getModel(), request, response);
+        executeTestWith("POST", "/users");
         assertEquals("/users", response.getRedirectedUrl());
     }
 
     @Test
     public void home() throws Exception {
-        MockHttpServletRequest request = new MockHttpServletRequest("GET", "/");
-        HandlerExecution execution = handlerMapping.getHandler(request);
-        ModelAndView mav = execution.handle(request, response);
-        mav.getView().render(mav.getModel(), request, response);
+        executeTestWith("GET", "/");
         assertEquals("home.jsp", response.getForwardedUrl());
     }
 
     @Test
     public void userForm() throws Exception {
-        MockHttpServletRequest request = new MockHttpServletRequest("GET", "/users/form");
-        HandlerExecution execution = handlerMapping.getHandler(request);
-        ModelAndView mav = execution.handle(request, response);
-        mav.getView().render(mav.getModel(), request, response);
+        executeTestWith("GET", "/users/form");
         assertEquals("/user/form.jsp", response.getForwardedUrl());
     }
 
     @Test
     public void loginForm() throws Exception {
-        MockHttpServletRequest request = new MockHttpServletRequest("GET", "/users/loginForm");
+        executeTestWith("GET", "/users/loginForm");
+        assertEquals("/user/login.jsp", response.getForwardedUrl());
+    }
+
+    private void executeTestWith(String requestMethod, String requestUrl) throws Exception {
+        MockHttpServletRequest request = new MockHttpServletRequest(requestMethod, requestUrl);
         HandlerExecution execution = handlerMapping.getHandler(request);
         ModelAndView mav = execution.handle(request, response);
         mav.getView().render(mav.getModel(), request, response);
-        assertEquals("/user/login.jsp", response.getForwardedUrl());
     }
 }
